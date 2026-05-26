d2568963a57686e0ac8a21fab8515fbb49ea9d82
deathstarset <anisbaziz.sub@gmail.com>
2026-05-26 20:48:18 +0100

    Add groq support and ui update

--- frontend/src/store/pdf-state.ts
+++ frontend/src/store/pdf-state.ts
@@ -3,13 +3,13 @@ import { File as FileType } from "@/types/db";
 type PdfState = {
   file: FileType | null;
   processed: boolean;
-  setFile: (file: FileType) => void;
+  setFile: (file: FileType | null) => void;
 };
 
 const usePdfStore = create<PdfState>((set) => ({
   file: null,
   processed: false,
-  setFile: (file: FileType) => set(() => ({ file })),
+  setFile: (file: FileType | null) => set(() => ({ file })),
 }));
 
 export default usePdfStore;
